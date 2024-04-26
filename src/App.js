ff56d1dfed59a87470de71238e1c3210513d87df
kamrulcse404 <kamrul.h01960@gmail.com>
2024-04-26 16:53:23 +0600

    sort by packed and description

--- src/App.js
+++ src/App.js
@@ -81,10 +81,27 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+
+  if (sortBy === 'input') {
+    sortedItems = items; 
+  }
+
+  if (sortBy === 'description') {
+    sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  }
+  
+  if (sortBy === 'packed') {
+    sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -95,10 +112,10 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
       </ul>
 
       <div className="actions">
-        <select>
-          <option value = "input">Sort by input order</option>
-          <option value = "description">Sort by description</option>
-          <option value = "packed">Sort by packed status</option>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
         </select>
       </div>
     </div>
